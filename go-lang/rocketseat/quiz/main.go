710dc33c6de7fa05784d309b678b0c3ac309b531
Anderson Marlon <anderson18.marlon@gmail.com>
2026-03-05 08:48:15 -0300

    Update: quiz/main.go

--- go-lang/rocketseat/quiz/main.go
+++ go-lang/rocketseat/quiz/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Question struct {
@@ -35,7 +37,53 @@ func (g *GameState) Init() {
 	fmt.Printf("Vamos ao jogo %s", g.Name)
 }
 
+func (g *GameState) ProcessCSV(){
+    file, err := os.Open("quiz-go.csv")
+    
+    if err != nil {
+        panic("Erro ao ler arquivo ou arquivo não encontrado")
+    }
+    
+    defer file.Close()
+    
+    reader := csv.NewReader(file)
+    records, err := reader.ReadAll()
+    
+    if err != nil {
+        panic("Error ao ler o documento .csv")
+    }
+    
+    for index, record := range records {
+        fmt.Println(index, record)
+        if index > 0 {
+            question := Question { 
+                Text: record[0],
+                Options: record[1:5],
+                Answer: toInt(record[5]),
+            }
+            
+            g.Questions = append(g.Questions, question)
+        }
+    }
+}
+
+func (g *GameState) Run() {
+    for index, question := range g.Questions {
+        fmt.Printf("\033[35m %d %s \033[35m\n", index+1, question.Text)
+    }
+}
+
 func main() {
 	game := &GameState{}
+	go game.ProcessCSV()
 	game.Init()
+	game.Run()
+}
+
+func toInt(number string) int {
+    index, err := strconv.Atoi(number)
+    if err != nil {
+        panic(err)
+    }
+    return index
 }
